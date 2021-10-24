a813e74f6952e0eaa944c0025805fee63b2e38f1
jessicacfsb <jessica.cfsb@gmail.com>
2021-10-24 16:58:21 -0300

    atividade02

---
+++ atividade02.py
@@ -0,0 +1,16 @@
+#Strings dinamicos
+
+#desafio 1 🥇
+nome = 'Jéssica'
+email = 'jessica@gmail.com'
+print (f'Olá {nome}, você cadastrou o email {email}, esta informação está correta?')
+
+#desafio 2 🥇
+b = 'ba'
+parte2 = 'nica'
+a = 'a'
+r = 'ri'
+parte1 = 'eletrô'
+t = 'te'
+
+print (f"\'{b}{t}{r}{a} {parte1}{parte2}'") 
